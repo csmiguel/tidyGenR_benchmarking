77950d9ace895de5bfc31e815d5f509de69acdcc
Miguel Camacho <miguelcamachosanchez@gmail.com>
2025-04-04 13:38:28 +0200

    add summary-tables.html

--- code/00_render.sh
+++ code/00_render.sh
@@ -4,3 +4,8 @@ quarto render
 export PATH="/usr/local/texlive/2024/bin/universal-darwin:$PATH"
 export PATH="/Applications/RStudio.app/Contents/Resources/app/quarto/bin/tools/x86_64:$PATH"
 Rscript -e "rmarkdown::render('supplementary_figures.Rmd', output_dir = 'sup-figs')"
+
+wait
+
+rm supplementary_figures.aux
+rm -rf tmp*
